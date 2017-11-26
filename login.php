494a7514337af8952cf7ad9d4549268ccb481398
r4v37t <r4v37t@gmail.com>
2017-11-26 12:52:45 +0700

    update halaman login

---
+++ login.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en" class="fullscreen-bg">
+<head>
+		<title>E-Kinerja</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+		<meta http-equiv="cache-control" content="max-age=0" />
+		<meta http-equiv="cache-control" content="no-cache" />
+		<meta http-equiv="expires" content="0" />
+		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+		<meta http-equiv="pragma" content="no-cache" />
+		<!-- VENDOR CSS -->
+		<link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="assets/vendor/themify-icons/css/themify-icons.css">
+		<!-- MAIN CSS -->
+		<link rel="stylesheet" href="assets/css/main.min.css">
+		<!-- GOOGLE FONTS -->
+		<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+		<!-- ICONS -->
+		<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+		
+	</head>
+	<body>
+		<!-- WRAPPER -->
+		<div id="wrapper">
+			<div class="vertical-align-wrap">
+				<div class="vertical-align-middle">
+					<div class="auth-box">
+						<div class="left">
+							<div class="content">
+								<div class="header">
+									<div class="logo text-center">
+										<img src="assets/img/logo.png" alt="Logo DPUPRPKP" width="64">
+									</div>
+									<p class="lead">Masuk E-Kinerja</p>
+								</div>
+								<form class="form-auth-small" method="post">
+									<div class="form-group">
+										<label for="idpengguna" class="control-label sr-only">ID Pengguna</label>
+										<input type="text" class="form-control" id="idpengguna" placeholder="Masukkan ID Pengguna">
+									</div>
+									<div class="form-group">
+										<label for="katasandi" class="control-label sr-only">Kata Sandi</label>
+										<input type="password" class="form-control" id="katasandi" placeholder="Masukkan Kata Sandi">
+									</div>
+									<button type="submit" class="btn btn-primary btn-lg btn-block">MASUK</button>
+								</form>
+							</div>
+						</div>
+						<div class="right">
+							<div class="overlay"></div>
+							<div class="content text">
+								<h1 class="heading">E-Kinerja</h1>
+								<p>DPUPRPKP</p>
+								<p>Kabupaten Kapuas, Kalimantan Tengah</p>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- END WRAPPER -->
+	</body>
+
+</html>
